a6e44b7c202d0c5c14989824c9f045ba6f2d460b
ProGamerx26 <cinarciibrahim5@gmail.com>
2026-04-21 12:34:35 +0300

    Add camera control and stock management functions
    Implement basic functions for camera control and stock management.

---
+++ script.js
@@ -0,0 +1,14 @@
+// Karekod ve Kamera Kontrol Sistemi
+function kamerayiAc() {
+    alert("Kamera hazırlanıyor... Lütfen izin veriniz.");
+    // Burada kamerayı başlatacak kütüphaneyi çağıracağız
+}
+
+function stokEkle(kod) {
+    console.log("Karekod okundu: " + kod);
+    // Okunan koda göre ürünü bulup stoğa ekleyecek
+}
+
+function karekodSatis(kod) {
+    // Okunan koda göre ürünü bulup kasadan düşecek
+}
